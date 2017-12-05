c91862eab970e2ca4c568585d550a9c499379c18
psimoes <p.simoes@campus.fct.unl.pt>
2017-12-05 01:36:07 +0000

    need game class

--- main.py
+++ main.py
@@ -46,11 +46,11 @@ you=Player(name,amount)
 exit=0
 which_deck=1
 
-while you.can_play() && exit == 0:
+while you.can_play() and exit==0:
     try:
-        bet= float(raw_input("how much you want to bet?"))
+        bet=float(raw_input("how much you want to bet?"))
         
-        if bet > p.balance():
+        if bet > you.balance():
             bet=10.0 
     except ValueError:
         bet=10.0
@@ -60,14 +60,12 @@ while you.can_play() && exit == 0:
     print("Bet of "+str(bet))
     deck=Deck(ReadDeck(str(which_deck)))
     #gives two cards
-    for i in range(0,1):
+    for i in range(0,2):
         p.append(deck.popfromdeck())
         d.append(deck.popfromdeck())
     
     showgame(p,d)
-    if isBlackJack(p):
-    
-    else:
+    print("HIT OR STAND?")
 
 # for i in range(0, 10):
 #     y=x.popfromdeck()
--- player.py
+++ player.py
@@ -2,21 +2,21 @@
 
 
 class Player:
-    def __init__(self,name,balance):
+    def __init__(self,name,amount):
         self.name = name
-        self.balance = balance
+        self.amount = amount
         self.hand=0
     
     #no verification is being made for negative balance
     def update_balance(self,x):
-        self.balance+=x
+        self.amount+=x
     #if balance >=10  player can play
     def can_play(self):
         return self.balance >= 10
     
     #player balance
     def balance(self):
-        return self.balance
+        return self.amount
     #player hand in one game
     def hand(self):
         return self.hand
@@ -24,5 +24,5 @@ class Player:
     def emptyhand(self):
         return self.hand
     
-    def addtohand(self,v)
-        self.hand+v
+    def addtohand(self,v):
+        self.hand+=v
